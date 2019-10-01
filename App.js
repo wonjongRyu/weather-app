41166497d4d53207e9599bb4b854889abf870ce8
wonjongRyu <wonjong4773@gmail.com>
2019-10-01 10:51:35 +0900

    Flexbox

--- App.js
+++ App.js
@@ -4,16 +4,22 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hello!</Text>
+      <View style={styles.yellowView} />
+      <View style={styles.blueView} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  yellowView: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: "yellow"
+  },
+  blueView: {
+    flex: 3,
+    backgroundColor: "blue"
   }
 });
